3bafe16f3c99f2ff9f75698401511b0d692bda35
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-09-01 17:48:30 +0800

    Update get_trial.py

--- get_trial.py
+++ get_trial.py
@@ -14,8 +14,8 @@ from utils import (clear_files, g0, keep, list_file_paths, list_folder_paths,
 
 def get_sub(session: PanelSession, opt: dict, cache: dict[str, list[str]]):
     url = cache['sub_url'][0]
-    #suffix = ' - ' + g0(cache, 'name')
-    suffix = ' - ' + 'TG频道@mfbpn'
+    #suffix = ' - TG频道@mfbpn' + g0(cache, 'name')
+    #suffix = ' - ' + 'TG频道@mfbpn'
     if 'speed_limit' in opt:
         suffix += ' ⚠️限速 ' + opt['speed_limit']
     try:
